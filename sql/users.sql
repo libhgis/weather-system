10836348a7c3999181812ac20cfcb4ee89fa1c9a
libh <libhgis@gmail.com>
2024-05-13 12:57:28 +0800

    sql files

---
+++ sql/users.sql
@@ -0,0 +1,8 @@
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES (3, 'root1', 'qqq@qq.com', '123123123');
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES (4, 'root11', '111@qq.com', '123123123');
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES (5, 'root2', '123456@qq.com', '123123123');
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES (6, 'root4', '1234567@qq.com', '123123123');
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES (7, 'root44', '12345611@qq.com', '123123123');
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES (8, 'root444', '123456111@qq.com', '123123123');
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES (9, 'root4555', '1234561111@qq.com', '123123123');
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES (10, 'rroot', 'aaa1@qq.com', '123123123');
